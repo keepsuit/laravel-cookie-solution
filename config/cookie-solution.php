2463bd7c8f9a02c6e2be70a5177d6102156e4bc9
Fabio Capucci <fabio.cappuc@gmail.com>
2023-01-16 12:32:09 +0100

    updated config & removed unused classes

--- config/cookie-solution.php
+++ config/cookie-solution.php
@@ -1,6 +1,18 @@
 <?php
 
-// config for Keepsuit/CookieSolution
 return [
+    /**
+     * Enable or disable the cookie solution.
+     */
+    'enabled' => env('COOKIE_SOLUTION_ENABLED', true),
 
+    /**
+     * Name of the cookie where we store the user's consent.
+     */
+    'cookie_name' => 'laravel_cookie_solution',
+
+    /**
+     * The cookie's lifetime in days.
+     */
+    'cookie_lifetime' => 365,
 ];
--- src/CookieSolutionServiceProvider.php
+++ src/CookieSolutionServiceProvider.php
@@ -10,16 +10,9 @@ class CookieSolutionServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-cookie-solution')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-cookie-solution_table')
-            ->hasCommand(CookieSolutionCommand::class);
+            ->hasViews();
     }
 }
