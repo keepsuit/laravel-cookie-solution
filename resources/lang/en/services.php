fc8dcf1fc3cf7be6dcc410818e61b6fc923399e6
Fabio Capucci <fabio.cappuc@gmail.com>
2023-03-21 12:17:39 +0100

    reordered services translations alphabetically

--- resources/lang/en/services.php
+++ resources/lang/en/services.php
@@ -1,15 +1,11 @@
 <?php
 
 return [
-    'google_analytics_4' => [
-        'description' => 'Google Analytics is a statistics service and uses cookies to collect data on website visitors. The data collected by Google Analytics is anonymous and does not include personal information. Google may use the data collected to contextualize and personalize the ads of its advertising network. You can get more information about Google Analytics and how to opt out of data collection by visiting the Google Analytics website.',
-        'cookies' => [
-            '_ga' => 'Used to distinguish users.',
-            '_ga_*' => 'Used to maintain session status.',
-        ],
+    'amazon_web_services' => [
+        'description' => 'Amazon Web Services (AWS) is hosting service that allows us to store and process data in a secure and reliable manner. Data stored on AWS may include personal information such as names, email addresses, and browsing data. Amazon may use the data in accordance with its own privacy policy, which can be found on the Amazon Web Services website. We take reasonable steps to ensure that data stored on AWS is protected and secure, but we cannot guarantee the absolute security of data stored on third-party servers.',
     ],
-    'google_tag_manager' => [
-        'description' => 'Google Tag Manager is a service that allows to add and manage various marketing and analytics tags through a single interface. Google Tag Manager does not collect any personal information, but it may pass data to other services such as Google Analytics, Adwords, and others. You can learn more about Google Tag Manager and its data practices by visiting the Google Tag Manager website.',
+    'digital_ocean' => [
+        'description' => 'DigitalOcean is a hosting service that allows us to store and process data in a secure and reliable manner. Data stored on DigitalOcean may include personal information such as names, email addresses, and browsing data. DigitalOcean may use the data in accordance with its own privacy policy, which can be found on the DigitalOcean website.',
     ],
     'facebook_pixel' => [
         'description' => 'Facebook Pixel uses cookies to collect data about website visitors. The data collected by Facebook Pixel is anonymous and does not include personal information. Facebook may use the collected data to target ads to website visitors and to understand the effectiveness of Facebook advertising campaigns. You can learn more about Facebook Pixel and how to opt out of data collection by visiting the Facebook website.',
@@ -17,12 +13,6 @@ return [
             '_fbp' => 'Used to store and track visits across websites.',
             'fr' => 'Used to provide ad delivery or retargeting.',
         ],
-    ],
-    'amazon_web_services' => [
-        'description' => 'Amazon Web Services (AWS) is hosting service that allows us to store and process data in a secure and reliable manner. Data stored on AWS may include personal information such as names, email addresses, and browsing data. Amazon may use the data in accordance with its own privacy policy, which can be found on the Amazon Web Services website. We take reasonable steps to ensure that data stored on AWS is protected and secure, but we cannot guarantee the absolute security of data stored on third-party servers.',
-    ],
-    'paypal' => [
-        'description' => 'PayPal is a payment service that allows us to accept payments from our customers. PayPal may collect personal information such as names, email addresses, and billing information.',
     ],
     'google_ads_remarketing' => [
         'description' => 'Google Ads Remarketing is a service that allows us to show ads to users who have previously visited our website. Google Ads Remarketing uses cookies to collect data about website visitors. The data collected by Google Ads Remarketing is anonymous and does not include personal information. Google may use the collected data to target ads to website visitors and to understand the effectiveness of Google advertising campaigns. You can learn more about Google Ads Remarketing and how to opt out of data collection by visiting the Google Ads website.',
@@ -35,13 +25,24 @@ return [
             '_gac_*' => 'Used to store and track audience reach',
         ],
     ],
+    'google_analytics_4' => [
+        'description' => 'Google Analytics is a statistics service and uses cookies to collect data on website visitors. The data collected by Google Analytics is anonymous and does not include personal information. Google may use the data collected to contextualize and personalize the ads of its advertising network. You can get more information about Google Analytics and how to opt out of data collection by visiting the Google Analytics website.',
+        'cookies' => [
+            '_ga' => 'Used to distinguish users.',
+            '_ga_*' => 'Used to maintain session status.',
+        ],
+    ],
+    'google_tag_manager' => [
+        'description' => 'Google Tag Manager is a service that allows to add and manage various marketing and analytics tags through a single interface. Google Tag Manager does not collect any personal information, but it may pass data to other services such as Google Analytics, Adwords, and others. You can learn more about Google Tag Manager and its data practices by visiting the Google Tag Manager website.',
+    ],
     'mailchimp' => [
         'description' => 'Mailchimp is an email marketing service that allows us to send emails to our customers. Mailchimp may collect personal information such as names, email addresses, and browsing data.',
+    ],
+    'paypal' => [
+        'description' => 'PayPal is a payment service that allows us to accept payments from our customers. PayPal may collect personal information such as names, email addresses, and billing information.',
     ],
     'zoho_campaigns' => [
         'description' => 'Zoho Campaigns is a marketing automation service that allows us to send emails to our customers. Zoho Campaigns may collect personal information such as names, email addresses, and browsing data.',
     ],
-    'digital_ocean' => [
-        'description' => 'DigitalOcean is a hosting service that allows us to store and process data in a secure and reliable manner. Data stored on DigitalOcean may include personal information such as names, email addresses, and browsing data. DigitalOcean may use the data in accordance with its own privacy policy, which can be found on the DigitalOcean website.',
-    ],
+
 ];
--- resources/lang/it/services.php
+++ resources/lang/it/services.php
@@ -1,15 +1,11 @@
 <?php
 
 return [
-    'google_analytics_4' => [
-        'description' => 'Google Analytics è un servizio di statistica e utilizza i cookie per raccogliere dati sui visitatori del sito web. I dati raccolti da Google Analytics sono anonimi e non includono informazioni personali. Google potrebbe utilizzare i dati raccolti per contestualizzare e personalizzare gli annunci del proprio network pubblicitario. È possibile ottenere ulteriori informazioni su Google Analytics e su come disattivare la raccolta di dati visitando il sito web di Google Analytics.',
-        'cookies' => [
-            '_ga' => 'Utilizzato per distinguere gli utenti.',
-            '_ga_*' => 'Utilizzato per mantenere lo stato della sessione.',
-        ],
+    'amazon_web_services' => [
+        'description' => "Amazon Web Services (AWS) è un servizio di hosting che ci consente di archiviare ed elaborare i dati in modo sicuro e affidabile. I dati archiviati su AWS possono includere informazioni personali come nomi, indirizzi e-mail e dati di navigazione. Amazon può utilizzare i dati in conformità con la propria politica sulla privacy, che può essere trovata sul sito web di Amazon Web Services. Adottiamo misure ragionevoli per garantire che i dati archiviati su AWS siano protetti e sicuri, ma non possiamo garantire l'assoluta sicurezza dei dati archiviati su server di terze parti.",
     ],
-    'google_tag_manager' => [
-        'description' => "Google Tag Manager è un servizio che consente di aggiungere e gestire diversi tag di marketing e analisi attraverso un'unica interfaccia. Google Tag Manager non raccoglie alcuna informazione personale, ma può trasferire i dati ad altri servizi come Google Analytics, Adwords e altri. Puoi saperne di più su Google Tag Manager e sulle sue pratiche relative ai dati visitando il sito web di Google Tag Manager.",
+    'digital_ocean' => [
+        'description' => 'DigitalOcean è un servizio di hosting che ci consente di archiviare ed elaborare i dati in modo sicuro e affidabile. I dati archiviati su DigitalOcean possono includere informazioni personali come nomi, indirizzi e-mail e dati di navigazione. DigitalOcean può utilizzare i dati in conformità con la propria politica sulla privacy, che può essere trovata sul sito web di DigitalOcean.',
     ],
     'facebook_pixel' => [
         'description' => "Facebook Pixel utilizza i cookie per raccogliere dati sui visitatori del sito web. I dati raccolti da Facebook Pixel sono anonimi e non includono informazioni personali. Facebook può utilizzare i dati raccolti per indirizzare gli annunci ai visitatori del sito Web e per comprendere l'efficacia delle campagne pubblicitarie di Facebook. Puoi saperne di più su Facebook Pixel e su come rinunciare alla raccolta dei dati visitando il sito web di Facebook.",
@@ -17,12 +13,6 @@ return [
             '_fbp' => 'Utilizzato per memorizzare e tracciare le visite su siti Web.',
             'fr' => 'Utilizzato per gli annunci o il retargeting.',
         ],
-    ],
-    'amazon_web_services' => [
-        'description' => "Amazon Web Services (AWS) è un servizio di hosting che ci consente di archiviare ed elaborare i dati in modo sicuro e affidabile. I dati archiviati su AWS possono includere informazioni personali come nomi, indirizzi e-mail e dati di navigazione. Amazon può utilizzare i dati in conformità con la propria politica sulla privacy, che può essere trovata sul sito web di Amazon Web Services. Adottiamo misure ragionevoli per garantire che i dati archiviati su AWS siano protetti e sicuri, ma non possiamo garantire l'assoluta sicurezza dei dati archiviati su server di terze parti.",
-    ],
-    'paypal' => [
-        'description' => 'PayPal è un servizio di pagamento che ci consente di accettare pagamenti dai nostri clienti. PayPal potrebbe raccogliere informazioni personali come nomi, indirizzi e-mail e informazioni di fatturazione.',
     ],
     'google_ads_remarketing' => [
         'description' => 'Google Ads Remarketing è un servizio che ci consente di mostrare annunci agli utenti che hanno precedentemente visitato il nostro sito web. Google Ads Remarketing utilizza i cookie per raccogliere dati sui visitatori del sito web. I dati raccolti da Google Ads Remarketing sono anonimi e non includono informazioni personali. Google potrebbe utilizzare i dati raccolti per indirizzare gli annunci ai visitatori del sito web e comprendere l\'efficacia delle campagne pubblicitarie di Google. È possibile ottenere ulteriori informazioni su Google Ads Remarketing e su come disattivare la raccolta di dati visitando il sito web di Google Ads.',
@@ -35,13 +25,24 @@ return [
             '_gac_*' => 'Utilizzato per memorizzare e monitorare la portata del pubblico',
         ],
     ],
+    'google_analytics_4' => [
+        'description' => 'Google Analytics è un servizio di statistica e utilizza i cookie per raccogliere dati sui visitatori del sito web. I dati raccolti da Google Analytics sono anonimi e non includono informazioni personali. Google potrebbe utilizzare i dati raccolti per contestualizzare e personalizzare gli annunci del proprio network pubblicitario. È possibile ottenere ulteriori informazioni su Google Analytics e su come disattivare la raccolta di dati visitando il sito web di Google Analytics.',
+        'cookies' => [
+            '_ga' => 'Utilizzato per distinguere gli utenti.',
+            '_ga_*' => 'Utilizzato per mantenere lo stato della sessione.',
+        ],
+    ],
+    'google_tag_manager' => [
+        'description' => "Google Tag Manager è un servizio che consente di aggiungere e gestire diversi tag di marketing e analisi attraverso un'unica interfaccia. Google Tag Manager non raccoglie alcuna informazione personale, ma può trasferire i dati ad altri servizi come Google Analytics, Adwords e altri. Puoi saperne di più su Google Tag Manager e sulle sue pratiche relative ai dati visitando il sito web di Google Tag Manager.",
+    ],
     'mailchimp' => [
         'description' => 'Mailchimp è un servizio di email marketing che ci permette di inviare email ai nostri clienti. Mailchimp potrebbe raccogliere informazioni personali come nomi, indirizzi e-mail e dati di navigazione.',
+    ],
+    'paypal' => [
+        'description' => 'PayPal è un servizio di pagamento che ci consente di accettare pagamenti dai nostri clienti. PayPal potrebbe raccogliere informazioni personali come nomi, indirizzi e-mail e informazioni di fatturazione.',
     ],
     'zoho_campaigns' => [
         'description' => 'Zoho Campaigns è un servizio di marketing che ci consente di inviare e-mail ai nostri clienti. Zoho Campaigns potrebbe raccogliere informazioni personali come nomi, indirizzi e-mail e dati di navigazione.',
     ],
-    'digital_ocean' => [
-        'description' => 'DigitalOcean è un servizio di hosting che ci consente di archiviare ed elaborare i dati in modo sicuro e affidabile. I dati archiviati su DigitalOcean possono includere informazioni personali come nomi, indirizzi e-mail e dati di navigazione. DigitalOcean può utilizzare i dati in conformità con la propria politica sulla privacy, che può essere trovata sul sito web di DigitalOcean.',
-    ],
+
 ];
