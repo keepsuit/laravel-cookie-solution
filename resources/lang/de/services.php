d3709173b34078d28e84301676a11411396ed0a1
Fabio Capucci <fabio.cappuc@gmail.com>
2024-02-06 14:59:39 +0100

    de translations

---
+++ resources/lang/de/services.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'amazon_web_services' => [
+        'description' => 'Amazon Web Services (AWS) ist ein Hosting-Dienst, der es uns ermöglicht, Daten auf sichere und zuverlässige Weise zu speichern und zu verarbeiten. Auf AWS gespeicherte Daten können persönliche Informationen wie Namen, E-Mail-Adressen und Browsing-Daten enthalten. Amazon kann die Daten gemäß seiner eigenen Datenschutzrichtlinie verwenden, die auf der Website von Amazon Web Services zu finden ist. Wir ergreifen angemessene Maßnahmen, um sicherzustellen, dass auf AWS gespeicherte Daten geschützt und sicher sind, können jedoch die absolute Sicherheit von auf Servern Dritter gespeicherten Daten nicht garantieren.',
+    ],
+    'digital_ocean' => [
+        'description' => 'DigitalOcean ist ein Hosting-Dienst, der es uns ermöglicht, Daten auf sichere und zuverlässige Weise zu speichern und zu verarbeiten. Auf DigitalOcean gespeicherte Daten können persönliche Informationen wie Namen, E-Mail-Adressen und Browsing-Daten enthalten. DigitalOcean kann die Daten gemäß seiner eigenen Datenschutzrichtlinie verwenden, die auf der Website von DigitalOcean zu finden ist.',
+    ],
+    'facebook_pixel' => [
+        'description' => 'Facebook Pixel verwendet Cookies, um Daten über Website-Besucher zu sammeln. Die von Facebook Pixel gesammelten Daten sind anonym und enthalten keine persönlichen Informationen. Facebook kann die gesammelten Daten verwenden, um Anzeigen an Website-Besucher zu richten und die Wirksamkeit von Facebook-Werbekampagnen zu verstehen. Weitere Informationen zu Facebook Pixel und wie Sie der Datensammlung widersprechen können, finden Sie auf der Facebook-Website.',
+        'cookies' => [
+            '_fbp' => 'Wird verwendet, um Besuche auf Websites zu speichern und zu verfolgen.',
+            'fr' => 'Wird verwendet, um Anzeigen auszuliefern oder neu zu zielen.',
+        ],
+    ],
+    'google_ads_remarketing' => [
+        'description' => 'Google Ads Remarketing ist ein Dienst, der es uns ermöglicht, Anzeigen für Benutzer anzuzeigen, die unsere Website zuvor besucht haben. Google Ads Remarketing verwendet Cookies, um Daten über Website-Besucher zu sammeln. Die von Google Ads Remarketing gesammelten Daten sind anonym und enthalten keine persönlichen Informationen. Google kann die gesammelten Daten verwenden, um Anzeigen an Website-Besucher zu richten und die Wirksamkeit von Google-Werbekampagnen zu verstehen. Weitere Informationen zu Google Ads Remarketing und wie Sie der Datensammlung widersprechen können, finden Sie auf der Google Ads-Website.',
+        'cookies' => [
+            '_generic' => 'Wird verwendet, um Anzeigen auszuliefern oder neu zu zielen.',
+            'NID' => 'Wird verwendet, um Anzeigen auszuliefern oder neu zu zielen und die Benutzereinstellungen zu speichern.',
+            'CONSENT' => 'Wird verwendet, um Zustimmungseinstellungen zu speichern.',
+            'HSID' => 'Wird verwendet, um Betrugsprävention bereitzustellen.',
+            'SID' => 'Wird verwendet, um Anzeigen auszuliefern oder neu zu zielen und Betrugsprävention bereitzustellen.',
+            '_gac_*' => 'Wird verwendet, um die Reichweite des Publikums zu speichern und zu verfolgen.',
+        ],
+    ],
+    'google_analytics_4' => [
+        'description' => 'Google Analytics ist ein Statistikdienst und verwendet Cookies, um Daten über Website-Besucher zu sammeln. Die von Google Analytics gesammelten Daten sind anonym und enthalten keine persönlichen Informationen. Google kann die gesammelten Daten verwenden, um Anzeigen seines Werbenetzwerks zu kontextualisieren und zu personalisieren. Weitere Informationen zu Google Analytics und wie Sie der Datensammlung widersprechen können, finden Sie auf der Google Analytics-Website.',
+        'cookies' => [
+            '_ga' => 'Wird verwendet, um Benutzer zu unterscheiden.',
+            '_ga_*' => 'Wird verwendet, um den Sitzungsstatus aufrechtzuerhalten.',
+        ],
+    ],
+    'google_tag_manager' => [
+        'description' => 'Google Tag Manager ist ein Dienst, der es ermöglicht, verschiedene Marketing- und Analysetags über eine einzige Schnittstelle hinzuzufügen und zu verwalten. Google Tag Manager erhebt keine persönlichen Informationen, kann jedoch Daten an andere Dienste wie Google Analytics, Adwords und andere weitergeben. Weitere Informationen zu Google Tag Manager und seinen Datenpraktiken finden Sie auf der Website von Google Tag Manager.',
+    ],
+    'klarna' => [
+        'description' => 'Klarna ist ein Zahlungsdienst, der es uns ermöglicht, Ratenzahlungen von unseren Kunden anzunehmen. Klarna kann persönliche Informationen wie Namen, E-Mail-Adressen und Rechnungsinformationen sammeln, um die Eignung des Benutzers für seine Zahlungsmethoden zu bewerten und diese Zahlungsmethoden zu personalisieren.',
+    ],
+    'mailchimp' => [
+        'description' => 'Mailchimp ist ein E-Mail-Marketingdienst, der es uns ermöglicht, E-Mails an unsere Kunden zu senden. Mailchimp kann persönliche Informationen wie Namen, E-Mail-Adressen und Browsing-Daten sammeln.',
+    ],
+    'paypal' => [
+        'description' => 'PayPal ist ein Zahlungsdienst, der es uns ermöglicht, Zahlungen von unseren Kunden anzunehmen. PayPal kann persönliche Informationen wie Namen, E-Mail-Adressen und Rechnungsinformationen sammeln.',
+    ],
+    'scalapay' => [
+        'description' => 'Scalapay ist ein Zahlungsdienst, der es uns ermöglicht, Ratenzahlungen von unseren Kunden anzunehmen. Scalapay kann persönliche Informationen wie Namen, E-Mail-Adressen und Rechnungsinformationen sammeln, um die Eignung des Benutzers für seine Zahlungsmethoden zu bewerten und diese Zahlungsmethoden zu personalisieren.',
+    ],
+    'stripe' => [
+        'description' => 'Stripe ist ein Zahlungsdienst, der es uns ermöglicht, Zahlungen von unseren Kunden anzunehmen. Stripe kann persönliche Informationen wie Namen, E-Mail-Adressen und Rechnungsinformationen sammeln.',
+    ],
+    'zoho_campaigns' => [
+        'description' => 'Zoho Campaigns ist ein Marketing-Automatisierungsdienst, der es uns ermöglicht, E-Mails an unsere Kunden zu senden. Zoho Campaigns kann persönliche Informationen wie Namen, E-Mail-Adressen und Browsing-Daten sammeln.',
+    ],
+];
---
+++ resources/lang/de/texts.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'banner' => [
+        'tab_consent' => 'Zustimmung',
+        'tab_customize' => 'Anpassen',
+        'tab_information' => 'Information',
+
+        'button_accept_all' => 'Alle akzeptieren',
+        'button_accept_selected' => 'Ausgewählte akzeptieren',
+        'button_refuse' => 'Ablehnen',
+        'button_customize' => 'Anpassen',
+        'button_show_banner' => 'Cookie-Einstellungen öffnen',
+
+        'consent_title' => 'Diese Website verwendet Cookies',
+        'consent_message' => 'Wir verwenden Cookies, um Inhalte und Anzeigen zu personalisieren, Funktionen für soziale Medien bereitzustellen und unseren Datenverkehr zu analysieren. Wir teilen auch Informationen über Ihre Nutzung unserer Website mit unseren Partnern für soziale Medien, Werbung und Analyse, die diese möglicherweise mit anderen Informationen kombinieren, die Sie ihnen zur Verfügung gestellt haben oder die sie aus Ihrer Nutzung ihrer Dienste gesammelt haben.',
+    ],
+
+    'cookie_policy' => [
+        'title' => 'Cookie-Richtlinie',
+        'cookies_list' => 'Verwendete Cookies-Liste',
+
+        'purpose_necessary' => 'Erforderlich',
+        'purpose_necessary_description' => 'Diese Cookies sind für die Funktionalität der Website erforderlich (z. B. Zugriff auf den persönlichen Bereich oder Speichern Ihres Warenkorbs). Die Website kann ohne diese Cookies nicht ordnungsgemäß funktionieren.',
+        'purpose_preferences' => 'Präferenzen',
+        'purpose_preferences_description' => 'Diese Cookies ermöglichen es der Website, sich an von Ihnen getroffene Entscheidungen zu erinnern (z. B. Ihre bevorzugte Sprache oder Ihren Standort) und erweiterte, persönlichere Funktionen bereitzustellen.',
+        'purpose_statistics' => 'Statistiken',
+        'purpose_statistics_description' => 'Diese Cookies sammeln Informationen darüber, wie Besucher die Website nutzen (z. B. welche Seiten Besucher am häufigsten besuchen). Die gesammelten Informationen sind aggregiert und anonym.',
+        'purpose_marketing' => 'Marketing',
+        'purpose_marketing_description' => 'Diese Cookies werden verwendet, um Werbung anzuzeigen, die für Sie und Ihre Interessen relevanter ist. Sie werden auch verwendet, um die Anzahl der Anzeigen zu begrenzen, die Sie sehen, sowie um die Wirksamkeit der Werbekampagne zu messen. Sie ermöglichen es Herausgebern und Drittanbieter-Werbetreibenden, Anzeigen auf Sie und Ihre Interessen auszurichten.',
+    ],
+
+    'privacy_policy' => [
+        'title' => 'Datenschutzrichtlinie',
+        'services_list' => 'Verwendete Dienste',
+        'service_data_processing_location' => 'Datenverarbeitungsstandort:',
+        'data_processing_owner' => 'Eigentümer der Datenverarbeitung',
+        'data_processing_owner_email' => 'E-Mail des Dateninhabers:',
+    ],
+];
