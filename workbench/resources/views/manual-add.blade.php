bc8a70e66496cffdfb747411221171d7bcb0f734
Fabio Capucci <fabio.cappuc@gmail.com>
2025-05-29 11:33:03 +0200

    dont add cookie solution banner if already present in the dom

---
+++ workbench/resources/views/manual-add.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <title>
+        Cookie Solution manual
+    </title>
+    @include('cookie-solution::script')
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="font-sans antialiased">
+<main class="max-w-screen-lg mx-auto px-4 grid gap-16 py-16">
+   <div id="cookie">
+       <cookie-solution-banner></cookie-solution-banner>
+   </div>
+</main>
+</body>
+</html>
