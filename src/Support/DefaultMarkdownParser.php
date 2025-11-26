ff5c4c6ccfe9bff5dd5f5b80d2ce60a493108b10
Fabio Capucci <fabio.cappuc@gmail.com>
2025-11-26 15:58:20 +0100

    use common mark directly

--- src/Support/DefaultMarkdownParser.php
+++ src/Support/DefaultMarkdownParser.php
@@ -3,41 +3,40 @@
 namespace Keepsuit\CookieSolution\Support;
 
 use Illuminate\Support\HtmlString;
-use Illuminate\Support\Str;
 use Keepsuit\CookieSolution\Contracts\MarkdownParser;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 use League\CommonMark\Extension\CommonMark\Node\Block\ListBlock;
 use League\CommonMark\Extension\CommonMark\Node\Block\ListItem;
 use League\CommonMark\Extension\DefaultAttributes\DefaultAttributesExtension;
+use League\CommonMark\MarkdownConverter;
 use League\CommonMark\Node\Block\Paragraph;
 
 class DefaultMarkdownParser implements MarkdownParser
 {
     public function parse(string $content): HtmlString
     {
-        $html = Str::markdown(
-            $content,
-            options: [
-                'default_attributes' => [
-                    Heading::class => [
-                        'part' => fn (Heading $node) => sprintf('headings h%d', $node->getLevel()),
-                    ],
-                    Paragraph::class => [
-                        'part' => 'p',
-                    ],
-                    ListBlock::class => [
-                        'part' => fn (ListBlock $node) => $node->getListData()->type === ListBlock::TYPE_ORDERED ? 'ol' : 'ul',
-                    ],
-                    ListItem::class => [
-                        'part' => 'li',
-                    ],
+        $environment = new Environment([
+            'default_attributes' => [
+                Heading::class => [
+                    'part' => fn (Heading $node) => sprintf('headings h%d', $node->getLevel()),
+                ],
+                Paragraph::class => [
+                    'part' => 'p',
+                ],
+                ListBlock::class => [
+                    'part' => fn (ListBlock $node) => $node->getListData()->type === ListBlock::TYPE_ORDERED ? 'ol' : 'ul',
+                ],
+                ListItem::class => [
+                    'part' => 'li',
                 ],
             ],
-            extensions: [
-                new DefaultAttributesExtension,
-            ]
-        );
+        ]);
 
-        return new HtmlString($html);
+        $environment->addExtension(new CommonMarkCoreExtension);
+        $environment->addExtension(new DefaultAttributesExtension);
+
+        return new HtmlString((string) (new MarkdownConverter($environment))->convert($content));
     }
 }
